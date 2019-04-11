a297763073d8fd8590d5aa410e67ad1d50fc3148
Matthew Crotty <mjc2011@verizon.net>
2019-04-11 16:59:34 -0400

    formatting

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,22 +5,28 @@ $(function(){
     switch(page){
         case "index.html":
             document.getElementById("1").style.fontWeight = "Bold";
+            document.getElementById("1").style.textDecoration = "underline";
             break;
         case "body1.html":
             document.getElementById("2").style.fontWeight = "Bold";
-                break;
+            document.getElementById("2").style.textDecoration = "underline";
+            break;
         case "body2.html":
             document.getElementById("3").style.fontWeight = "Bold";
-                break;
+            document.getElementById("3").style.textDecoration = "underline";
+            break;
         case "body3.html":
             document.getElementById("4").style.fontWeight = "Bold";
-                break;
+            document.getElementById("4").style.textDecoration = "underline";
+            break;
         case "sources.html":
             document.getElementById("5").style.fontWeight = "Bold";
-                break;
+            document.getElementById("5").style.textDecoration = "underline";
+            break;
         case "about.html":
             document.getElementById("6").style.fontWeight = "Bold";
-                break;
+            document.getElementById("6").style.textDecoration = "underline";
+            break;
 
     }
 });
